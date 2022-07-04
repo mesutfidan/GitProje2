57766d26af89077396ced8bbdf200de17bb2101b
mesutfidan <mesut_fidan@outlook.com>
2022-07-04 21:47:34 +0300

    2. bölüm yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -2,5 +2,6 @@ public class Ogrenci {
     public static void main(String[] args) {
         System.out.println("merhaba dunya");
         System.out.println("merhaba mesut");
+        System.out.println("hi mesut");
     }
 }
